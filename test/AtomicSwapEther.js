7821a2d27de3a4575ed661eef0929c9f1f6adab5
willclarktech <willclarktech@users.noreply.github.com>
2019-03-18 17:59:36 +0100

    Add test for AtomicSwapEther.viewSwap()

--- test/AtomicSwapEther.js
+++ test/AtomicSwapEther.js
@@ -12,6 +12,8 @@ chai.use(chaiAsPromised);
 contract("AtomicSwapEther", accounts => {
   const defaultPreimage = "0x42a990655bffe188c9823a2f914641a32dcbb1b28e8586bd29af291db7dcd4e8";
   const defaultHash = "0x261c74f7dd1ed6a069e18375ab2bee9afcb1095613f53b07de11829ac66cdfcc";
+  const nullPreimage = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  const defaultValue = 0;
 
   let testContract;
 
@@ -173,6 +175,62 @@ contract("AtomicSwapEther", accounts => {
       await testContract.abort(id);
 
       await expect(testContract.abort(id)).to.be.rejectedWith(/no open swap found for the given id/i);
+    });
+  });
+
+  describe("viewSwap", () => {
+    it("shows an open swap by id", async () => {
+      const id = makeRandomID();
+      const timeout = makeTimeout();
+      const recipient = accounts[0];
+
+      await testContract.open(id, recipient, defaultHash, timeout);
+
+      const result = await testContract.viewSwap(id);
+      expect(result.recipient).to.equal(recipient);
+      expect(result.hash).to.equal(defaultHash);
+      expect(result.timeout.toNumber()).to.equal(timeout);
+      expect(result.value.toNumber()).to.equal(defaultValue);
+      expect(result.preimage).to.equal(nullPreimage);
+    });
+
+    it("shows a claimed swap by id including preimage", async () => {
+      const id = makeRandomID();
+      const timeout = makeTimeout();
+      const recipient = accounts[0];
+
+      await testContract.open(id, recipient, defaultHash, timeout);
+      await testContract.claim(id, defaultPreimage);
+
+      const result = await testContract.viewSwap(id);
+      expect(result.recipient).to.equal(recipient);
+      expect(result.hash).to.equal(defaultHash);
+      expect(result.timeout.toNumber()).to.equal(timeout);
+      expect(result.value.toNumber()).to.equal(defaultValue);
+      expect(result.preimage).to.equal(defaultPreimage);
+    });
+
+    it("shows an aborted swap by id", async () => {
+      const id = makeRandomID();
+      const timeout = makeTimeout(1);
+      const recipient = accounts[0];
+
+      await testContract.open(id, recipient, defaultHash, timeout);
+      await sleep(2);
+      await testContract.abort(id);
+
+      const result = await testContract.viewSwap(id);
+      expect(result.recipient).to.equal(recipient);
+      expect(result.hash).to.equal(defaultHash);
+      expect(result.timeout.toNumber()).to.equal(timeout);
+      expect(result.value.toNumber()).to.equal(defaultValue);
+      expect(result.preimage).to.equal(nullPreimage);
+    });
+
+    it("errors when attempting to view a non-existent swap", async () => {
+      const id = makeRandomID();
+
+      await expect(testContract.viewSwap(id)).to.be.rejectedWith(/no swap found for the given id/i);
     });
   });
 });
