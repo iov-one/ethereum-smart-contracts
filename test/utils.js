17d2ffaf0f1dd81d9fd308bf12022eccd4bdb6dc
willclarktech <willclarktech@users.noreply.github.com>
2019-03-18 15:49:05 +0100

    Add test utils

---
+++ test/utils.js
@@ -0,0 +1,29 @@
+const crypto = require("crypto");
+const { Algorithm } = require("@iov/bcp-types");
+const { ethereumCodec } = require("@iov/ethereum");
+
+function makeRandomID() {
+  return `0x${crypto.randomBytes(32).toString("hex")}`;
+}
+
+function makeRandomAddress() {
+  const dataPrefix = 0x04;
+  return ethereumCodec.identityToAddress({
+    chainId: "dummy-chain",
+    pubkey: {
+      algo: Algorithm.Secp256k1,
+      data: new Uint8Array([dataPrefix, ...crypto.randomBytes(64)]),
+    },
+  });
+}
+
+function makeTimeout(seconds = 100) {
+  const currentUnixTimestamp = Math.floor(Date.now() / 1000);
+  return currentUnixTimestamp + seconds;
+}
+
+module.exports = {
+  makeRandomID,
+  makeRandomAddress,
+  makeTimeout,
+};
