e79784ed4a0b924e5cc842f9e1de2485534882a2
willclarktech <willclarktech@users.noreply.github.com>
2019-03-18 16:29:24 +0100

    Add test for AtomicSwapEther.claim()

--- test/AtomicSwapEther.js
+++ test/AtomicSwapEther.js
@@ -9,7 +9,8 @@ const atomicSwap = artifacts.require("./AtomicSwapEther.sol");
 const { expect } = chai;
 chai.use(chaiAsPromised);
 
-contract("AtomicSwapEther", (/*accounts*/) => {
+contract("AtomicSwapEther", accounts => {
+  const defaultPreimage = "0x42a990655bffe188c9823a2f914641a32dcbb1b28e8586bd29af291db7dcd4e8";
   const defaultHash = "0x261c74f7dd1ed6a069e18375ab2bee9afcb1095613f53b07de11829ac66cdfcc";
 
   let testContract;
@@ -49,5 +50,77 @@ contract("AtomicSwapEther", (/*accounts*/) => {
         );
       }
     });
+  });
+
+  describe("claim()", () => {
+    it("emits a Claimed event", async () => {
+      const id = makeRandomID();
+      const timeout = makeTimeout();
+
+      await testContract.open(id, accounts[0], defaultHash, timeout);
+
+      const { tx } = await testContract.claim(id, defaultPreimage);
+
+      await expectEvent.inTransaction(tx, atomicSwap, "Claimed", {
+        id,
+        preimage: defaultPreimage,
+      });
+    });
+
+    it("errors when attempting to claim a non-existent swap", async () => {
+      const id = makeRandomID();
+
+      await expect(testContract.claim(id, defaultPreimage)).to.be.rejectedWith(
+        /no open swap found for the given id/i,
+      );
+    });
+
+    it("errors when attempting to claim a swap with incorrect preimage", async () => {
+      const preimage = `0x5${defaultPreimage.slice(3)}`;
+      const id = makeRandomID();
+      const timeout = makeTimeout();
+
+      await testContract.open(id, accounts[0], defaultHash, timeout);
+
+      await expect(testContract.claim(id, preimage)).to.be.rejectedWith(/invalid preimage for swap hash/i);
+    });
+
+    it("errors when attempting to claim a swap with a preimage which is too short", async () => {
+      // 31 bytes
+      const preimage = "0xa990655bffe188c9823a2f914641a32dcbb1b28e8586bd29af291db7dcd4e8";
+      const hash = "0xe4632a45b8e39230777acdb63647b9513d5686bb4d9cb7a3be2f89664eb0fd32";
+      const id = makeRandomID();
+      const timeout = makeTimeout();
+
+      await testContract.open(id, accounts[0], hash, timeout);
+
+      await expect(testContract.claim(id, preimage)).to.be.rejectedWith(/invalid preimage for swap hash/i);
+    });
+
+    it("errors when attempting to claim a swap with a preimage which is too long", async () => {
+      // 33 bytes
+      const preimage = "0xa990655bffe188c9823a2f914641a32dcbb1b28e8586bd29af291db7dcd4e8";
+      const hash = "0xe4632a45b8e39230777acdb63647b9513d5686bb4d9cb7a3be2f89664eb0fd32";
+      const id = makeRandomID();
+      const timeout = makeTimeout();
+
+      await testContract.open(id, accounts[0], hash, timeout);
+
+      await expect(testContract.claim(id, preimage)).to.be.rejectedWith(/invalid preimage for swap hash/i);
+    });
+
+    it("errors when attempting to claim a swap which has already been claimed", async () => {
+      const id = makeRandomID();
+      const timeout = makeTimeout();
+
+      await testContract.open(id, accounts[0], defaultHash, timeout);
+      await testContract.claim(id, defaultPreimage);
+
+      await expect(testContract.claim(id, defaultPreimage)).to.be.rejectedWith(
+        /no open swap found for the given id/i,
+      );
+    });
+
+    it("errors when attempting to claim a swap which has already been aborted");
   });
 });
