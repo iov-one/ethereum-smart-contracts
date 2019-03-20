a8b90cc3fbe5db3254a0ba15112e4f358b134a70
Simon Warta <simon@kullo.net>
2019-03-20 23:13:49 +0100

    Inline flatten command

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -6,13 +6,9 @@ outdir="build/flattened"
 
 mkdir -p "$outdir"
 
-function flatten() {
-  ./node_modules/.bin/truffle-flattener "$1" --output "$2"
-}
-
 for inpath in contracts/*.sol; do
   filename=$(basename "$inpath" .sol)
   outpath="$outdir/$filename.flattened.sol"
   echo "Flattening $inpath into $outpath ..."
-  flatten "$inpath" "$outpath"
+  ./node_modules/.bin/truffle-flattener "$inpath" --output "$outpath"
 done
