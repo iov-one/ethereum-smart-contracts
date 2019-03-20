7a893c9957ff75cccaa632f94ce7dba5deffa306
Simon Warta <simon@kullo.net>
2019-03-20 23:07:42 +0100

    Move mkdir -p "$outdir" out of loop

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -4,14 +4,14 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 
 outdir="build/flattened"
 
+mkdir -p "$outdir"
+
 function flatten() {
   ./node_modules/.bin/truffle-flattener "$1" --output "$2"
 }
 
 for inpath in contracts/*.sol; do
   filename=$(basename "$inpath" .sol)
-
-  mkdir -p "$outdir"
   outpath="$outdir/$filename.flattened.sol"
   echo "Flattening $inpath into $outpath ..."
   flatten "$inpath" "$outpath"
