636d3bc19a1a1a663133c5a2632652582f77f513
willclarktech <willclarktech@users.noreply.github.com>
2019-03-26 14:05:20 +0100

    Add CI setup

---
+++ scripts/ci/main.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck > /dev/null && shellcheck "$0"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck disable=SC1090
+source "$SCRIPT_DIR/_includes.sh";
+
+fold_start "yarn-install"
+retry 3 yarn install
+fold_end
+
+fold_start "lint-js"
+yarn run lint:js
+fold_end
+
+fold_start "lint-sol"
+yarn run lint:sol
+fold_end
+
+fold_start "build"
+yarn run build
+fold_end
+
+fold_start "test"
+yarn run test
+fold_end
+
+fold_start "verify"
+yarn run verify --no-progress
+fold_end
