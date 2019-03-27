c360fa776aa7447cd74bceeee7c78ed776e99065
willclarktech <willclarktech@users.noreply.github.com>
2019-03-27 09:56:23 +0100

    Tidy test utils

--- test/utils.js
+++ test/utils.js
@@ -2,9 +2,13 @@ const BN = require("bn.js");
 const crypto = require("crypto");
 const { EthereumConnection, toChecksummedAddress } = require("@iov/ethereum");
 
+const TEST_URL = "http://localhost:8545";
+
 async function getBalance(address) {
-  const connection = await EthereumConnection.establish("http://localhost:8545");
+  const connection = await EthereumConnection.establish(TEST_URL);
   const account = await connection.getAccount({ address });
+  connection.disconnect();
+
   const ethWallet = account && account.balance.find(({ tokenTicker }) => tokenTicker === "ETH");
   const balance = ethWallet ? ethWallet.quantity : "0";
   return new BN(balance, 10);
@@ -20,8 +24,10 @@ function makeRandomAddress() {
 }
 
 async function makeTimeout(blocks = 2) {
-  const connection = await EthereumConnection.establish("http://localhost:8545");
+  const connection = await EthereumConnection.establish(TEST_URL);
   const currentHeight = await connection.height();
+  connection.disconnect();
+
   return currentHeight + blocks;
 }
 
